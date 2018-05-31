7720234e7b36bbc348f5bea28794d58db204761a
NEA-I <evgeny.nuzhdin@indusoft.ru>
2018-05-31 07:43:28 +0300

    1

---
+++ 001/a.js
@@ -0,0 +1,13 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('a');
+        console.log('b');
+        console.log('c');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=a.js.map
---
+++ 001/a.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        console.log('a');
+        console.log('b');
+        console.log('c');
+        return 0;
+    }
+}
+
+Startup.main();
